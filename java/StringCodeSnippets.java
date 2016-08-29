3cc74688011eb3eff43f46c1141161a4d328f0fb
Marc-André Girard <marcandr.girard@gmail.com>
2016-08-29 11:08:18 -0400

    String formatting

--- java/StringCodeSnippets.java
+++ java/StringCodeSnippets.java
@@ -4,5 +4,6 @@ public class StringCodeSnippets {
 
 	public static void startsWith() {
 		StringUtils.startsWith("String you want to test that may be null", "String");
+		MessageFormat.format("String is \"{1}\", number is {0}.", 42, "foobar"); //It's not StringInterpolation but well
 	}
 }
